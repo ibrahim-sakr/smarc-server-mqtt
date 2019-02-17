f46a8c2bb822a1b2756e737376526cd8e36849f7
Ibrahim Sakr <ebrahimes@gmail.com>
2019-02-17 10:08:22 +0200

    convert APIs to GraphQL and add SSL encryption

--- config/mqtt.config.ts
+++ config/mqtt.config.ts
@@ -1,9 +1,18 @@
 const mosca = require('mosca');
 
 export default {
-    port: 9090,
+    // port: 9090,
     persistence: {
         factory: mosca.persistence.Mongo,
         url: 'mongodb://localhost:27017/mqtt'
+    },
+    logger: {
+        name: "secureExample",
+        level: 40,
+    },
+    secure: {
+        port: 9091,
+        keyPath: __dirname + '/../ssl/smarc-root-ca.key',
+        certPath: __dirname + '/../ssl/smarc-root-cert.pem',
     }
 };
